22287734e130a922a1967af3bda41d1c8d01cf5d
conisoube <conniesoubelet@gmail.com>
2022-05-28 14:52:39 +0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponenteNavBarComponent } from './componente-nav-bar/componente-nav-bar.component';
+import { ComponenteFooterComponent } from './componente-footer/componente-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponenteNavBarComponent
+    ComponenteNavBarComponent,
+    ComponenteFooterComponent
   ],
   imports: [
     BrowserModule,
