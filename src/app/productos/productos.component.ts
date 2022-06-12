353cb46a5b529ff3bcdcb3ed713980526334a3eb
marielvejar <mariel.vejar@bennu.cl>
2022-06-12 19:33:02 -0400

    mas cosas agregadas

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiserviceService} from '../apiservice.service';
 import { CartService } from '../service/cart.service';
-
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-productos',
@@ -10,25 +10,29 @@ import { CartService } from '../service/cart.service';
 })
 export class ProductosComponent implements OnInit {
 
-  constructor(private service: ApiserviceService, private cartService : CartService) { }
+  constructor(private sanitizer: DomSanitizer, private service: ApiserviceService, private cartService : CartService) { }
 
   readData: any;
   successmsg:any;
+
+
 
   ngOnInit(): void {
       this.service.getAllData().subscribe((res)=>{
         console.log(res,"res==>");
         ///NO LO BORRRES JESUSCRISTO
         this.readData = res.data;
-
         this.readData.forEach((a:any)=>{
           Object.assign(a,{cantidad:1,total:a.precio});
         });
       });
+
+
   }
   addtocomanda(item: any){
       this.cartService.addtoCart(item);
       this.cartService.setProducto(item)
   }
+
 
 }
