5bcc9e8778672547eef5e695e6c44f7b4a5080d1
marielvejar <mariel.vejar@bennu.cl>
2022-06-13 21:52:44 -0400

    cambios mas

---
+++ src/app/service/mesa.service.ts.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Mesa.Service.TsService {
+
+  constructor(private service: ApiserviceService) { }
+}
